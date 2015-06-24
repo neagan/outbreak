f55cb1c8e73dac042de89e024d44a7e2a2c9264c
Nick Eagan <neagan26@gmail.com>
2015-06-24 15:07:07 -0700

    create canvas element

--- app/js/app.js
+++ app/js/app.js
@@ -3,17 +3,27 @@
 module.exports = App;
 
 function App() {
-  this.x = 0;
+  this.x = 1;
+  this.canvas = document.getElementById('canvas');
+  this.ctx = this.canvas.getContext('2d');
 };
 
 App.prototype.run = function() {
   var _this = this;
 
   requestAnimationFrame(function() {
-    document.getElementById('log').innerHTML = _this.x;
-    _this.x++;
+
+    if (_this.x % 2 === 0) {
+      _this.ctx.fillStyle = 'green';
+    } else {
+      _this.ctx.fillStyle = 'blue';
+    }
+
+    _this.ctx.fillRect(10, 10, _this.x, 100);
 
     if (_this.x <= 100) {
+      console.log(_this.x);
+      _this.x++;
       _this.run();
     }
   });
